d0be78a5de2d8d2ba2e98339821ffa612da02846
yuma nacamura <yuma.nacamura@gmail.com>
2018-08-03 21:08:12 +0900

    失敗です

---
+++ gan_labeled_mnist/test.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import load_model
+from keras.utils import np_utils
+
+FILENAME = './models/gan-50000-iter.h5'
+
+model = load_model(FILENAME)
+
+print('OK')
+label = int(input())
+
+while label:
+    noise = np.random.uniform(-1,1,(1,100))
+    label = np_utils.to_categorical(label,10).reshape((1,10))
+    img = model.predict([noise,label]).reshape((28,28))
+    img = 0.5 * img + 0.5
+    print(img.shape)
+    plt.imshow(img)
+    plt.show()
+    label = int(input())
